de753e7b4f29d136966013dff51a6dd723978da9
KahlerYasla <garpayyasla@gmail.com>
2024-05-30 20:29:41 +0300

    mobile: structure fixed

---
+++ clients/mobile/constants/Theme.ts
@@ -0,0 +1,37 @@
+const COLORS = {
+    primary: "#312651",
+    secondary: "#444262",
+    tertiary: "#FF7754",
+};
+
+const FONT = {
+    regular: "UbuntuMonoRegular",
+    bold: "UbuntuMonoBold",
+    italic: "UbuntuMonoItalic",
+    boldItalic: "UbuntuMonoBoldItalic",
+};
+
+const SHADOWS = {
+    small: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 2,
+    },
+    medium: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 5.84,
+        elevation: 5,
+    },
+};
+
+export { COLORS, FONT, SHADOWS };
---
+++ clients/mobile/constants/VisualAssets.ts
@@ -0,0 +1,7 @@
+// images
+const logo = require('../assets/images/app/logo.png');
+
+// icons
+const marketMarker = require('../assets/icons/market-marker.png');
+
+export { logo, marketMarker };
